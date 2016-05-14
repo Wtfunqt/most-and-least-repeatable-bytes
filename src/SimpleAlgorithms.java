9c6a389861ff63de44481b1914f11112b27338b4
goadik <goadik@yandex.ru>
2016-05-14 09:49:56 -0400

    Least repeatable byte method added
    added least repeatable method as well as moved most repeatable method
    code inside a method

--- src/SimpleAlgorithms.java
+++ src/SimpleAlgorithms.java
@@ -10,18 +10,48 @@ import java.util.Map;
  * Find the byte with the most and least amount of repeats
  * Display them in a console
  */
-public class SimpleAlgorithms {
+public class SimpleAlgorithms {	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		FileInputStream fis = null;
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int max = 0;
 		
 		/*try catch block in order to catch an exception in case file name
 		is incorrect or file does not exist */
 		try {
 			fis = new FileInputStream(br.readLine());
 		} catch (FileNotFoundException e) {}
+		findMostRepeatable(fis);
+		findLeastRepeatable(fis);
+		
+		br.close();
+		fis.close();
+	}
+	public static void findLeastRepeatable(FileInputStream fis) throws IOException {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int min = Integer.MAX_VALUE;
+		
+		while (fis.available() > 0) {
+			int i = fis.read();
+			if (map.containsKey(i)) {
+				map.put(i, map.get(i) + 1);
+			} else
+				map.put(i, 0);
+		}
+		for (Map.Entry<Integer, Integer> entrySet : map.entrySet()) {
+			if (entrySet.getValue() != 0 && entrySet.getValue() < min) {
+				min = entrySet.getValue();
+			}
+		}
+		
+		for (Map.Entry<Integer, Integer> entrySet : map.entrySet()) {
+			if (entrySet.getValue() == min)
+				System.out.println(entrySet.getKey());
+		}
+	}
+	
+	public static void findMostRepeatable(FileInputStream fis) throws IOException {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int max = Integer.MIN_VALUE;
 		
 		while (fis.available() > 0) {
 			int i = fis.read();
@@ -38,9 +68,6 @@ public class SimpleAlgorithms {
 			if (entrySet.getValue() == max)
 				System.out.println(entrySet.getKey());
 		}
-		
-		br.close();
-		fis.close();
 	}
 }
 	
